04d2cd3a75879cf968d482fa407cb9b61978f191
25DeMayoFutbolClub <167278327+25DeMayoFutbolClub@users.noreply.github.com>
2024-04-17 02:05:19 -0300

    Add files via upload

---
+++ partidos.js
@@ -0,0 +1,13 @@
+var countDownDate = new Date("April 20, 2024 17:15:00").getTime();
+var countdownFunction = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  document.getElementById("days").innerHTML = Math.floor(distance / (1000 * 60 * 60 * 24));
+  document.getElementById("hours").innerHTML = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  document.getElementById("minutes").innerHTML = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  document.getElementById("seconds").innerHTML = Math.floor((distance % (1000 * 60)) / 1000);
+  if (distance < 0) {
+    clearInterval(countdownFunction);
+    document.getElementById("countdown").innerHTML = "¡Es hora!";
+  }
+}, 1000);
